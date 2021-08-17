ba3271a1066fc1f5209542da2e5dbe3e95359a1f
Atul Patil <atulpatil539@gmail.com>
2021-08-17 21:57:59 +0530

    feat: updated

--- istriangle.js
+++ istriangle.js
@@ -2,9 +2,24 @@ const inputs = document.querySelectorAll(".angle-input")
 const isTriangleBtn = document.querySelector("#is-triangle-btn")
 const outputEl = document.querySelector("#istriangle")
 
+function calculateSumOfAngles(angle1,angle2,angle3){
+    const sumOfAngles = angle1 + angle2 + angle3
+    // console.log(sumOfAngles)
+    return sumOfAngles
+}
+
 
 function isTriangle(){
-    console.log("hey there")
+    const sumOfAngles = calculateSumOfAngles(Number(inputs[0].value),Number(inputs[1].value),Number(inputs[2].value))
+    if (Number(inputs[0].value),Number(inputs[1].value),Number(inputs[2].value)){
+    if (sumOfAngles===180){
+        outputEl.innerText="Yeah! The angles form Triangle🍕"
+    }else{
+        outputEl.innerText= "Ohh! The angles don't form Triangle😮"
+    }
+}else{
+    outputEl.innerText= "Ohh No! Plz enter all the fields!!!"
+}
 }
 
 
