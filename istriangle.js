31be072ea56b289a5ecab6aebe2b5e20e9d5aad4
Atul Patil <atulpatil539@gmail.com>
2021-08-17 19:57:35 +0530

    feat:added

---
+++ istriangle.js
@@ -0,0 +1,11 @@
+const inputs = document.querySelectorAll(".angle-input")
+const isTriangleBtn = document.querySelector("#is-triangle-btn")
+const outputEl = document.querySelector("#istriangle")
+
+
+function isTriangle(){
+    console.log("hey there")
+}
+
+
+isTriangleBtn.addEventListener("click", isTriangle)
